604e01db7e38f37489735856860a30f6ffaf6009
Leandro Udala <leandroudala@gmail.com>
2020-10-21 22:34:44 -0300

    Added profiles

--- src/main/java/app/udala/forum/config/security/SecurityConfigurations.java
+++ src/main/java/app/udala/forum/config/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package app.udala.forum.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +19,7 @@ import app.udala.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -52,6 +54,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, "/actuator").permitAll()
 			.antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
 			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+			.antMatchers(HttpMethod.DELETE, "/topics/*").hasRole("MODERADOR")
 			.anyRequest().authenticated()
 			.and().csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES ('Aluno', 'aluno@email.com', '$2a$10$zMhOcUjo7viZ5pzkeSQnQ.k/Ji7rLVu4mC4ayWdsXb9Rq5hDr5wdG');
+INSERT INTO USUARIO(nome, email, senha) VALUES ('Moderador', 'moderador@email.com', '$2a$10$zMhOcUjo7viZ5pzkeSQnQ.k/Ji7rLVu4mC4ayWdsXb9Rq5hDr5wdG');
+
+INSERT INTO PERFIL(id, nome) VALUES (1, 'ROLE_ALUNO'), (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (1, 1), (2, 2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação'), ('HTML5', 'Front-end');
 
